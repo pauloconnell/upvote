cdb269fc9eb57b9401365c157378925487c1c865
Paul O'Connell <p_ollie@hotmail.com>
2024-08-26 20:00:30 -0700

    Update: added click functionality and CSS

--- src/components/Upvote/Upvote.js
+++ src/components/Upvote/Upvote.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react'; 
 import ArrowUpload from '../../icons/arrow-up.svg';
 import Plus from '../../icons/plus.svg';
+import './Upvote.css';
 
-class Upvote extends Component {
+function Upvote() {
 
+    
+        const [count, setCount] = useState(0);
+      
+        const handleUpvote = () => {
+          setCount(count + 1);
+        };
 
-
-  render() {
-    return <div><h2> Upvote component will go here</h2>
-        <section>
-        Arrow-upload /
-        <img src={ArrowUpload} alt="Arrow Upload Icon" />
-        Plus
-        <img src={Plus} alt="Plus Icon" />
-        </section>
-        </div>;
-  }
+        const renderVotes = () => {                 // return images to match number of upvotes
+            let jsx = [];
+            for(let x = 0; x < count; x++){
+                jsx.push(<img src={ArrowUpload} key = {x} alt="Arrow Upload Icon" /> ); 
+            }
+            return jsx;
+        }
+  
+    return(
+      <div><h2> Upvote component will go here</h2>
+        <div class="upvote-container">
+          Name to be Upvoted            {renderVotes()}  <img src={ArrowUpload} alt="Arrow Upload Icon" />
+          
+          <div onClick={handleUpvote} class="upvote-icon" ><img src={Plus} alt="Plus Icon"  /> </div>
+        </div>
+      </div>
+    );
+  
 }
 
 export default Upvote; 
